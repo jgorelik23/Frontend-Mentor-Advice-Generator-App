0c4e87b26f5d11c494cb700778dc557927fa4e5b
jgorelik23 <91423797+jgorelik23@users.noreply.github.com>
2022-04-25 20:11:48 -0400

    Add files via upload

---
+++ Front End Mentor | Advice Generator App/src/script.js
@@ -0,0 +1,18 @@
+const adviceAPI = "https://api.adviceslip.com/advice";
+
+const quoteEl = document.getElementById("quote-el");
+const adviceNumEl = document.getElementById("advice-num-el");
+
+const diceBtn = document.getElementById("dice-btn");
+
+function getAdvice() {
+    fetch(adviceAPI)
+        .then(response => response.json())
+        .then(data => {
+            quoteEl.textContent = `"${data.slip.advice}"`;
+            adviceNumEl.textContent = data.slip.id;
+        })
+        .catch(console.error);
+}
+
+diceBtn.addEventListener("click", getAdvice);
